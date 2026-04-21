81e3b5a4f4fa601f8dbb1d10e68def60cedddec9
jkwli <wright_joshua@wheatoncollege.edu>
2026-04-20 20:08:23 -0400

    Adding the story screen and level counter with screen switch functionality

--- PasswordGauntlet/app/src/main/java/com/example/passwordgauntlet/gameActivity.java
+++ PasswordGauntlet/app/src/main/java/com/example/passwordgauntlet/gameActivity.java
@@ -1,11 +1,13 @@
 package com.example.passwordgauntlet;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,12 +20,21 @@ public class gameActivity extends AppCompatActivity {
     EditText input;
     String inputText;
     Button Submit;
+    Button storyButton;
+    TextView levelText;
+
+    int level = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_game);
+
+        // Receive level from story screen
+        if (getIntent().hasExtra("level")) {
+            level = getIntent().getIntExtra("level", 1);
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -33,6 +44,11 @@ public class gameActivity extends AppCompatActivity {
 
         input = findViewById(R.id.input);
         Submit = findViewById(R.id.Submit);
+        storyButton = findViewById(R.id.storyButton);
+        levelText = findViewById(R.id.levelText);
+
+        // Display level
+        levelText.setText("Level " + level);
 
         Rules[] rules = { new minLength() };
 
@@ -54,6 +70,13 @@ public class gameActivity extends AppCompatActivity {
                     Submit.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FF5555")));
                 }
             }
+        });
+
+        storyButton.setOnClickListener(v -> {
+            Intent intent = new Intent(gameActivity.this, storyActivity.class);
+            intent.putExtra("level", level);
+            startActivity(intent);
+            finish();
         });
     }
 }
